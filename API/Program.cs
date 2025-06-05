fd7c3dc3a9e9420f008c6737ae49a84f340e763b
Eviwe Khohlombe <129647608+1996Woso@users.noreply.github.com>
2025-06-05 14:37:21 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,8 +13,17 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IUsersRepository, UsersRepository>();
-var app = builder.Build();
+//When Client and API are not from origin e.g localhost:5002 and localhost:5001
+builder.Services.AddCors();
 
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader()
+  .AllowAnyMethod()
+//   .WithOrigins("http://localhost:4200", "https://localhost:4200")
+  .AllowAnyOrigin()
+
+);
 app.MapControllers();
 
 app.Run();
